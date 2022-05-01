f58adb27e6b8998c61bb2241a4e162ee04a46f9b
Drew Brokamp <drew.brokamp@gmail.comm>
2022-05-01 15:47:42 -0400

    Refactored name files

--- src/main/java/com/javafxgit/javafxandgit/Controller/MainController.java
+++ src/main/java/com/javafxgit/javafxandgit/Controller/MainController.java
@@ -1,9 +1,9 @@
-package com.javafxgit.javafxandgit;
+package com.javafxgit.javafxandgit.Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
